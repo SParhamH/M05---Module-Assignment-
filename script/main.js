ab19bd3214414370bfd090bcb7c55ac810d3f09a
SParhamH <97205925+SParhamH@users.noreply.github.com>
2022-01-26 16:03:02 -0800

    Create main.js

---
+++ script/main.js
@@ -0,0 +1,10 @@
+var x = prompt("Enter you number:");           // Unit of table
+var operator = 'multiplication';              // Type of calculation
+var i = 1;                                   // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';  // Message
+if (operator === 'multiplication') {  		
+   while (i < 11) {
+   msg += i + ' x ' + Number(x) + ' = ' + (i * Number(x)) + '<br />';
+   i++;
+ 	 }
+}  
